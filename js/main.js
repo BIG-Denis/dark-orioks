55c6d91c9b5da26e7961e2902bd32f73a07ac91a
BIG-Denis <102750547+BIG-Denis@users.noreply.github.com>
2023-01-30 18:30:56 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,24 +1,17 @@
 
-function getElementByXpath(path) {
-    return document.evaluate(path, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
-}
 
 document.head.innerHTML += '<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@600&display=swap">';
 
-let select1 = getElementByXpath("/html/body/div[3]/div/div[2]/div[1]/div[5]/div[1]/select[1]");
-let select2 = getElementByXpath("/html/body/div[3]/div/div[2]/div[1]/div[5]/div[1]/select[2]");
 
-select1.insertAdjacentHTML('beforebegin', '	&nbsp;&nbsp;&nbsp;&nbsp;');
-select2.insertAdjacentHTML('beforebegin', '	&nbsp;&nbsp;&nbsp;&nbsp;');
-
-
-// work with select1
-
-select1.style = "margin-bottom: 12px; margin-right: 5px;";
-
-function work_with_select1() {
-    let select_ref = getElementByXpath("/html/body/div[3]/div/div[2]/div[1]/div[5]/div[1]/a");
-    document.location = select_ref.href;
+let selects = document.querySelectorAll('select.input-sm');
+if (selects.length > 0) {
+    let select1 = selects[0];
+    select1.insertAdjacentHTML('beforebegin', '&nbsp;&nbsp;&nbsp;&nbsp;');
+    selects[1].insertAdjacentHTML('beforebegin', '&nbsp;&nbsp;&nbsp;&nbsp;');
+    select1.style = "margin-bottom: 12px; margin-right: 5px;";
+    function work_with_select1() {
+        let select_ref = document.querySelectorAll('div.well.ng-binding a')[1];
+        document.location = select_ref.href;
+    }
+    select1.onchange = work_with_select1;
 }
-
-select1.onchange = work_with_select1;
