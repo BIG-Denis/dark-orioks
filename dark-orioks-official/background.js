51e905da4e2ed7e6da3665553cd8d399a0d05f83
BIG-Denis <102750547+BIG-Denis@users.noreply.github.com>
2023-10-07 21:20:01 +0300

    MEGA-FEGA 1.4.2 update ooooofffffffff

---
+++ dark-orioks-official/background.js
@@ -0,0 +1,32 @@
+let theme_names = [
+    "classic",
+    "dark_teal",
+    "african_morning",
+    "neon_bar",
+    "claybricks_area",
+    "twilight_wizard"
+];
+
+chrome.runtime.onInstalled.addListener(function() {
+    chrome.storage.sync.get("theme", function (opt) {
+        let theme = opt.theme;
+        
+        if (theme > 0) {
+            chrome.scripting.registerContentScripts([{
+                id: "content-scripts",
+                matches: ["https://orioks.miet.ru/*"],
+                excludeMatches: ["https://orioks.miet.ru/moodle/*"],
+                js: ["js/main.js", "js/" + theme_names[theme] + ".js"],
+                css: ["assets/css/main.css", "assets/css/" + theme_names[theme] + ".css", "assets/css/beaty_navbar.css"]
+            }]);
+        } else {
+            chrome.scripting.registerContentScripts([{
+                id: "content-scripts",
+                matches: ["https://orioks.miet.ru/*"],
+                excludeMatches: ["https://orioks.miet.ru/moodle/*"],
+                js: ["js/classic.js"],
+                css: ["assets/css/classic.css"]
+            }]);
+        }
+    });
+});
